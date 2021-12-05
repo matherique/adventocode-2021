6381dab269367973d3daa0efce732ebbf3f0148d
Matheus Henrique <matherique@gmail.com>
2021-12-05 16:07:52 -0300

    solve day 5 part 2

--- day5/solve.go
+++ day5/solve.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 )
 
-const input = "example"
-const size = 10
+const input = "in"
+const size = 1000
 
 func main() {
 	b, err := os.ReadFile(input)
@@ -22,40 +22,66 @@ func main() {
 	lines = lines[:len(lines)-1]
 
 	table := [size * size]int{}
+	// lines = []string{"9,7 -> 7,9", "1,1 -> 3,3"}
 
 	for _, line := range lines {
 		x1, y1, x2, y2 := parse(line)
+		// fmt.Printf("======= %s =======\n", line)
 
-		if x1 != x2 && y1 != y2 {
-			continue
-		}
-
-		table[idx(x1, y1)] += 1
-
-		// fmt.Println(x1, y1, x2, y2)
-
-		for {
-			switch true {
-			case x1 < x2:
-				x1 += 1
-			case x2 < x1:
-				x1 -= 1
-			case y1 < y2:
-				y1 += 1
-			case y2 < y1:
-				y1 -= 1
-			}
-
-			// fmt.Println(x1, y1, x2, y2)
+		if x1 == x2 || y1 == y2 {
 			table[idx(x1, y1)] += 1
 
-			if x1 == x2 && y1 == y2 {
-				break
+			// fmt.Println(x1, y1, x2, y2)
+
+			for {
+				switch true {
+				case x1 < x2:
+					x1 += 1
+				case x2 < x1:
+					x1 -= 1
+				case y1 < y2:
+					y1 += 1
+				case y2 < y1:
+					y1 -= 1
+				}
+
+				// fmt.Println(x1, y1, x2, y2)
+				table[idx(x1, y1)] += 1
+
+				if x1 == x2 && y1 == y2 {
+					break
+				}
 			}
+		} else {
+			table[idx(x1, y1)] += 1
+			for {
+				if x1 > x2 {
+					x1 -= 1
+				} else {
+					x1 += 1
+				}
+
+				if y1 > y2 {
+					y1 -= 1
+				} else {
+					y1 += 1
+				}
+
+				// fmt.Println(x1, y1, x2, y2)
+				table[idx(x1, y1)] += 1
+
+				if x1 == x2 && y1 == y2 {
+					break
+				}
+			}
+
 		}
+
 	}
 
-	var total int
+	show(table)
+
+	total := 0
 
 	for _, v := range table {
 		if v > 1 {
@@ -67,8 +93,21 @@ func main() {
 
 }
 
+func show(table [size * size]int) {
+	i := 0
+	for {
+		if size+i > len(table) {
+			break
+		}
+
+		fmt.Println(table[i : size+i])
+		i += size
+	}
+
+}
+
 func idx(x, y int) int {
-	return y + x*size
+	return x + y*size
 }
 
 func parse(line string) (int, int, int, int) {
